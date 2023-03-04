1caec2a8fc90dc874faac2cb3c65345058a83177
laakri <glassisaif@gmail.com>
2023-03-04 21:04:39 +0100

    change gmail backend

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -20,7 +20,7 @@ router.post("", (req, res, next) => {
       html: `
         <div style="font-family: sans-serif; max-width: 500px; margin: 0 auto; text-align: center; border: 1px solid #ccc; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
           <h1 style="color: #726fdd; font-size: 30px;">${req.body.name}</h1>
-          <p style="font-size: 18px; margin-bottom: 10px;">From: ${req.body.email}</p>
+          <p style="font-size: 18px; margin-bottom: 11px;">From: ${req.body.email}</p>
           <p style="font-size: 18px; margin-bottom: 20px;">Message:</p>
           <p style="font-size: 16px; line-height: 1.5; margin-bottom: 30px;">${req.body.textA}</p>
         </div>
