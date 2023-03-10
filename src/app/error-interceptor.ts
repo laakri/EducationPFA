39d76a51bb9d911e47d025997b3352d012b60381
laakri <glassisaif@gmail.com>
2023-03-10 12:04:21 +0100

    modify error handle

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,36 +1,35 @@
 import { ErrorComponent } from './error/error.component';
 import { Injectable } from '@angular/core';
-import {HttpInterceptor,HttpRequest,HttpHandler, HttpErrorResponse} from "@angular/common/http";
-import { throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
-import {MatSnackBar} from '@angular/material/snack-bar';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private _snackBar: MatSnackBar) {}
 
-  constructor(private _snackBar: MatSnackBar){}
-  
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
-
-      
-
-      catchError((error:HttpErrorResponse)=>{
-        let errorMessage ="An unknown error occurred!";
-        if (error.error.message){
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error.message) {
           errorMessage = error.error.message;
         }
-        
-        this._snackBar.openFromComponent(ErrorComponent,
-          {data :{message :errorMessage},
-          duration: 2500,
-          panelClass: ['red-snackbar']
-        });
 
+        this._snackBar.openFromComponent(ErrorComponent, {
+          data: { message: errorMessage },
+          duration: 2500,
+          panelClass: ['red-snackbar'],
+        });
 
         return throwError(error);
       })
     );
   }
-  
 }
